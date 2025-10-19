118858b8854579c5f8ba8fab643035c145fe0f29
Sajith Silva <sajiths@pagero.com>
2025-10-19 19:52:01 +0530

    more  changes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import {McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
 import {StdioServerTransport} from "@modelcontextprotocol/sdk/server/stdio.js";
 import z from "zod";
+import fs from "fs/promises";
+import path from "path";
+
+const dbPath = path.join(process.cwd(), "db.json");
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+};
+
+type Db = {
+    users: User[];
+};
 
 const server = new McpServer({
     name: "mcp-example1",
@@ -13,10 +27,29 @@ const server = new McpServer({
     }
 })
 
-function createUser({name, email}: {name: string, email: string}){
-    // Simulate user creation and return a user ID
-    return Math.floor(Math.random() * 10000);
-}
+server.prompt("create-random-starwars-user", "Create random star wars user", {}, ()=>{
+    return {
+        messages:[{
+            role: "user",
+            content: {
+                type: "text", text:"Create a random star wars user with a name and email"        
+            }
+        }]
+
+    }
+})
+
+server.resource("users", "user://all",{
+    description: "get all users",
+    title:"users",
+    mimeType: "application/json",
+},
+async url=>{
+    const db: Db = JSON.parse(await fs.readFile(dbPath, "utf-8"));
+    return {
+        contents: [{uri:url.href,text:JSON.stringify(db.users, null, 2), mimeType:"application/json"}],
+    }
+})
 
 server.tool("Create-User", "Creates a new user in the database", {
     name: z.string().describe("The name of the user to create"),
@@ -29,7 +62,7 @@ server.tool("Create-User", "Creates a new user in the database", {
     openWorldHint: true,
 }, async (p)=>{
     try {
-        const id = createUser(p)
+        const id = await createUser(p)
         return {
             content: [{
                 type: "text",
@@ -45,6 +78,19 @@ server.tool("Create-User", "Creates a new user in the database", {
         }
     }
 })
+
+async function createUser({name, email}: {name: string, email: string}){
+    const db: Db = JSON.parse(await fs.readFile(dbPath, "utf-8"));
+    const newUser: User = {
+        id: db.users.length + 1,
+        name,
+        email,
+    };
+    db.users.push(newUser);
+    await fs.writeFile(dbPath, JSON.stringify(db, null, 2));
+    return newUser.id;
+}
+
 
 async function main(){
     const transport = new StdioServerTransport()
